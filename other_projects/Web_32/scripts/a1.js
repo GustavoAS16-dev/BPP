4de4c227030a04102b4fea225f8842480c7302b9
GustavoAS16-dev <68358704+GustavoAS16-dev@users.noreply.github.com>
2022-11-15 16:16:53 -0300

    Add files via upload

--- other_projects/Web_32/scripts/a1.js
+++
@@ -1,4 +0,0 @@
-var s1 = prompt("Insira o número do seu cartão para acessar o site: ");
-var s2 = prompt("Insira a senha da sua conta: ");
-
-alert("Parabéns! Agora você conseguiu entrar no site!");
